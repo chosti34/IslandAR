0363cc933edcefada5353e3df67b45c5fbc2a99b
Timur Karimov <chosti34@gmail.com>
2018-11-26 12:18:03 +0300

    Добавлен обратный отсчёт

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+	Text m_text;
+	float m_seconds;
+
+	void Start()
+	{
+		m_text = GetComponent<Text>();
+		m_seconds = 120.0f;
+	}
+
+	void Update()
+	{
+		if (m_seconds > 0.0f)
+		{
+			m_seconds -= Time.deltaTime;
+		}
+		else
+		{
+			m_seconds = 0.0f;
+		}
+		m_text.text = string.Format("{0:D2}:{1:D2}", (int)(m_seconds / 60), (int)(m_seconds % 60));
+		UpdateTimerColor();
+	}
+
+	void UpdateTimerColor()
+	{
+		if (m_seconds >= 30.0f)
+		{
+			m_text.color = Color.white;
+		}
+		else if (m_seconds >= 10.0f)
+		{
+			m_text.color = Color.yellow;
+		}
+		else
+		{
+			m_text.color = Color.red;
+		}
+	}
+}
