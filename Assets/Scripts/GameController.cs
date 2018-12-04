4da5dba19041a30fed3d589c0691a27caae4cc15
Timur Karimov <chosti34@gmail.com>
2018-12-04 21:02:22 +0300

    Реализована механика передвижения персонажа путем ответа на вопросы

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+
+public class QuestionAndAnswer
+{
+	public QuestionAndAnswer(string question, string answer)
+	{
+		this.question = question;
+		this.answer = answer;
+	}
+
+	public string question;
+	public string answer;
+}
+
+public static class IListExtensions
+{
+	public static void Shuffle<T>(this IList<T> list)
+	{
+		var count = list.Count;
+		var last = count - 1;
+		for (var i = 0; i < last; ++i)
+		{
+			var r = UnityEngine.Random.Range(i, count);
+			var tmp = list[i];
+			list[i] = list[r];
+			list[r] = tmp;
+		}
+	}
+}
 
 public class GameController : MonoBehaviour
 {
@@ -9,6 +38,14 @@ public class GameController : MonoBehaviour
 	{
 		Pause,
 		Play
+	}
+
+	enum Direction
+	{
+		Up,
+		Right,
+		Down,
+		Left
 	}
 
 	State m_state;
@@ -22,12 +59,37 @@ public class GameController : MonoBehaviour
 	public GameObject m_germanPiratePrefab;
 
 	public GameObject m_answers;
+	public Button[] m_buttons;
 
 	public Terrain m_terrain;
+
 	private Pirate m_pirate;
+	private QuestionAndAnswer[] m_questionsAndAnswers;
+	private Dictionary<int, Direction> m_directionMap;
 
 	void Start()
 	{
+		if (m_buttons.Length != 4)
+		{
+			throw new System.Exception("GameController must have 4 buttons!");
+		}
+
+		m_questionsAndAnswers = new QuestionAndAnswer[10]
+		{
+			new QuestionAndAnswer("1 + 3 = ?", "4"),
+			new QuestionAndAnswer("1 - 3 = ?", "-2"),
+			new QuestionAndAnswer("10 + 1 = ?", "11"),
+			new QuestionAndAnswer("3 * 8 = ?", "24"),
+			new QuestionAndAnswer("8 / 4 = ?", "2"),
+			new QuestionAndAnswer("98 - 27 = ?", "71"),
+			new QuestionAndAnswer("1 + 4 = ?", "5"),
+			new QuestionAndAnswer("10 - 11 = ?", "-1"),
+			new QuestionAndAnswer("10 * 3 = ?", "30"),
+			new QuestionAndAnswer("7 + 7 = ?", "14")
+		};
+		m_directionMap = new Dictionary<int, Direction>();
+
+		m_answers.SetActive(false);
 		ActivatePauseState();
 	}
 
@@ -46,34 +108,27 @@ public class GameController : MonoBehaviour
 		HashSet<int> indices = new HashSet<int>();
 		while (indices.Count != 5)
 		{
-			int index = Random.Range(7, 42);
+			int index = UnityEngine.Random.Range(7, 42);
 			indices.Add(index);
 		}
 
 		foreach (int index in indices)
 		{
-			Vector3 rotation = new Vector3(0.0f, Random.Range(-360.0f, 360.0f), 0.0f);
+			Vector3 rotation = new Vector3(0.0f, UnityEngine.Random.Range(-360.0f, 360.0f), 0.0f);
 			Chest chest = Instantiate(m_chestPrefab, m_grid.GetCell(index).transform.position, Quaternion.Euler(rotation))
 				.GetComponent<Chest>();
 			chest.SetCellIndex(index);
 		}
 
 		// Spawn player
-		int randomCellIndex = Random.Range(0, 7);
+		int randomCellIndex = UnityEngine.Random.Range(0, 7);
 		m_pirate = Instantiate(m_germanPiratePrefab, m_grid.GetCell(randomCellIndex).transform.position, Quaternion.Euler(new Vector3(0, 0, 0)), m_terrain.transform)
 			.GetComponent<Pirate>();
 		m_pirate.SetCellIndex(randomCellIndex);
 
-		// Move answers
-		m_answers.transform.position = new Vector3(
-			m_pirate.transform.position.x,
-			m_answers.transform.position.y,
-			m_pirate.transform.position.z
-		);
-	}
-
-	private void Update()
-	{
+		m_answers.SetActive(true);
+		MoveAnswersToPirate();
+		UpdateAnswersAndQuestionsAvailability();
 	}
 
 	public void ActivatePauseState()
@@ -85,45 +140,66 @@ public class GameController : MonoBehaviour
 
 	public void OnFirstButtonClick()
 	{
-		int index = m_pirate.GetCellIndex();
-		GridCell upCell = m_grid.GetCell(index + 7);
-		m_pirate.MoveTo(upCell.transform.position);
-		m_pirate.SetCellIndex(index + 7);
-		m_pirate.transform.rotation = Quaternion.Euler(0, 0, 0);
-
-		// Move answers
-		m_answers.transform.position = new Vector3(
-			m_pirate.transform.position.x,
-			m_answers.transform.position.y,
-			m_pirate.transform.position.z
-		);
+		Debug.Log(m_directionMap[0]);
+		MovePirate(m_directionMap[0]);
 	}
 
 	public void OnSecondButtonClick()
 	{
-		int index = m_pirate.GetCellIndex();
-		GridCell rightCell = m_grid.GetCell(index + 1);
-		m_pirate.MoveTo(rightCell.transform.position);
-		m_pirate.SetCellIndex(index + 1);
-		m_pirate.transform.rotation = Quaternion.Euler(0, 90, 0);
-
-		// Move answers
-		m_answers.transform.position = new Vector3(
-			m_pirate.transform.position.x,
-			m_answers.transform.position.y,
-			m_pirate.transform.position.z
-		);
+		Debug.Log(m_directionMap[1]);
+		MovePirate(m_directionMap[1]);
 	}
 
 	public void OnThirdButtonClick()
 	{
-		int index = m_pirate.GetCellIndex();
-		GridCell downCell = m_grid.GetCell(index - 7);
-		m_pirate.MoveTo(downCell.transform.position);
-		m_pirate.SetCellIndex(index - 7);
-		m_pirate.transform.rotation = Quaternion.Euler(0, 180, 0);
+		Debug.Log(m_directionMap[2]);
+		MovePirate(m_directionMap[2]);
+	}
 
-		// Move answers
+	public void OnFourthButtonClick()
+	{
+		Debug.Log(m_directionMap[3]);
+		MovePirate(m_directionMap[3]);
+	}
+
+	void MovePirate(Direction direction)
+	{
+		int index = Grid.NON_REACHABLE_CELL;
+		Quaternion rotation = Quaternion.identity;
+
+		switch (direction)
+		{
+			case Direction.Up:
+				index = m_grid.GetUpperCellIndex(m_pirate.GetCellIndex());
+				rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+				break;
+			case Direction.Right:
+				index = m_grid.GetRightCellIndex(m_pirate.GetCellIndex());
+				rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+				break;
+			case Direction.Down:
+				index = m_grid.GetDownCellIndex(m_pirate.GetCellIndex());
+				rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+				break;
+			case Direction.Left:
+				index = m_grid.GetLeftCellIndex(m_pirate.GetCellIndex());
+				rotation = Quaternion.Euler(new Vector3(0, 270, 0));
+				break;
+		}
+
+		if (index != Grid.NON_REACHABLE_CELL)
+		{
+			GridCell cell = m_grid.GetCell(index);
+			m_pirate.MoveTo(cell.transform.position);
+			m_pirate.SetCellIndex(index);
+			m_pirate.transform.rotation = rotation;
+			MoveAnswersToPirate();
+			UpdateAnswersAndQuestionsAvailability();
+		}
+	}
+
+	void MoveAnswersToPirate()
+	{
 		m_answers.transform.position = new Vector3(
 			m_pirate.transform.position.x,
 			m_answers.transform.position.y,
@@ -131,19 +207,91 @@ public class GameController : MonoBehaviour
 		);
 	}
 
-	public void OnFourthButtonClick()
+	void UpdateAnswersAndQuestionsAvailability()
 	{
-		int index = m_pirate.GetCellIndex();
-		GridCell leftCell = m_grid.GetCell(index - 1);
-		m_pirate.MoveTo(leftCell.transform.position);
-		m_pirate.SetCellIndex(index - 1);
-		m_pirate.transform.rotation = Quaternion.Euler(0, 270, 0);
+		List<TextMesh> answersText = new List<TextMesh>();
+		for (int i = 0; i < m_answers.transform.childCount; ++i)
+		{
+			answersText.Add(m_answers.transform.GetChild(i).gameObject.GetComponentInChildren<TextMesh>());
+		}
 
-		// Move answers
-		m_answers.transform.position = new Vector3(
-			m_pirate.transform.position.x,
-			m_answers.transform.position.y,
-			m_pirate.transform.position.z
-		);
+		List<int> directionIndices = Enumerable.Range(0, 4).ToList();
+		List<int> questionsAndAnswersIndices = Enumerable.Range(0, 10).ToList();
+
+		for (int i = 0; i < m_buttons.Length; ++i)
+		{
+			int rndIdx = UnityEngine.Random.Range(0, directionIndices.Count);
+			int rnd = directionIndices[rndIdx];
+			directionIndices.RemoveAt(rndIdx);
+			m_directionMap[i] = ToDirection(rnd);
+
+			rndIdx = UnityEngine.Random.Range(0, questionsAndAnswersIndices.Count);
+			rnd = questionsAndAnswersIndices[rndIdx];
+			questionsAndAnswersIndices.RemoveAt(rndIdx);
+			QuestionAndAnswer questionAndAnswer = m_questionsAndAnswers[rnd];
+
+			m_buttons[i].GetComponentInChildren<Text>().text = questionAndAnswer.question;
+			answersText[ToDirectionIndex(m_directionMap[i])].text = questionAndAnswer.answer;
+		}
+
+		int upper = m_grid.GetUpperCellIndex(m_pirate.GetCellIndex());
+		int right = m_grid.GetRightCellIndex(m_pirate.GetCellIndex());
+		int down = m_grid.GetDownCellIndex(m_pirate.GetCellIndex());
+		int left = m_grid.GetLeftCellIndex(m_pirate.GetCellIndex());
+
+		m_buttons[GetButtonIndexAssociatedWithDirection(Direction.Up)].interactable = upper != Grid.NON_REACHABLE_CELL;
+		m_buttons[GetButtonIndexAssociatedWithDirection(Direction.Right)].interactable = right != Grid.NON_REACHABLE_CELL;
+		m_buttons[GetButtonIndexAssociatedWithDirection(Direction.Down)].interactable = down != Grid.NON_REACHABLE_CELL;
+		m_buttons[GetButtonIndexAssociatedWithDirection(Direction.Left)].interactable = left != Grid.NON_REACHABLE_CELL;
+
+		for (int i = 0; i < m_answers.transform.childCount; ++i)
+		{
+			m_answers.transform.GetChild(i).gameObject.SetActive(
+				m_buttons[GetButtonIndexAssociatedWithDirection(ToDirection(i))].interactable);
+		}
+	}
+
+	int GetButtonIndexAssociatedWithDirection(Direction direction)
+	{
+		foreach (KeyValuePair<int, Direction> pair in m_directionMap)
+		{
+			if (pair.Value.Equals(direction))
+			{
+				return pair.Key;
+			}
+		}
+		throw new System.Exception("no button associated with passed direction");
+	}
+
+	Direction ToDirection(int direction)
+	{
+		switch (direction)
+		{
+		case 0:
+			return Direction.Up;
+		case 1:
+			return Direction.Right;
+		case 2:
+			return Direction.Down;
+		case 3:
+			return Direction.Left;
+		}
+		throw new System.Exception("GetRandomDirection(): invalid direction's integer representation");
+	}
+
+	int ToDirectionIndex(Direction direction)
+	{
+		switch (direction)
+		{
+		case Direction.Up:
+			return 0;
+		case Direction.Right:
+			return 1;
+		case Direction.Down:
+			return 2;
+		case Direction.Left:
+			return 3;
+		}
+		throw new System.Exception("GetRandomDirection(): invalid direction representation");
 	}
 }
--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+	public static int NON_REACHABLE_CELL = -1;
 	public GridCell[] m_cells;
+	private int m_rows;
+
+	void Start()
+	{
+		m_rows = 7;
+		if (m_cells.Length != m_rows * m_rows)
+		{
+			throw new Exception("Game designed to have 49 cells");
+		}
+	}
 
 	public GridCell GetCell(int index)
 	{
-		if (index >= 0 && index <= 48)
+		if (CellIndexIsValid(index))
 		{
 			return m_cells[index];
 		}
@@ -18,6 +29,40 @@ public class Grid : MonoBehaviour
 
 	public GridCell GetCell(int row, int col)
 	{
-		return GetCell(row * 7 + col);
+		return GetCell(row * m_rows + col);
+	}
+
+	public int GetRowsCount()
+	{
+		return m_rows;
+	}
+
+	public int GetUpperCellIndex(int center)
+	{
+		int upper = center + m_rows;
+		return CellIndexIsValid(upper) ? upper : NON_REACHABLE_CELL;
+	}
+
+	public int GetRightCellIndex(int center)
+	{
+		int col = center % m_rows;
+		return (col == (m_rows - 1)) ? NON_REACHABLE_CELL : center + 1;
+	}
+
+	public int GetDownCellIndex(int center)
+	{
+		int down = center - m_rows;
+		return CellIndexIsValid(down) ? down : NON_REACHABLE_CELL;
+	}
+
+	public int GetLeftCellIndex(int center)
+	{
+		int col = center % m_rows;
+		return (col == 0) ? NON_REACHABLE_CELL : center - 1;
+	}
+
+	bool CellIndexIsValid(int index)
+	{
+		return index >= 0 && index < m_cells.Length;
 	}
 }
